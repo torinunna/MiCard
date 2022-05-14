d391a5daf7de36f6cb5b085f82b62e300c2ca672
torinunna <torinunna9588@gmail.com>
2022-05-14 23:11:38 +0900

    Initial Commit

---
+++ MiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiCard
+//
+//  Created by 권유진 on 2022/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiCard/MiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCardApp.swift
+//  MiCard
+//
+//  Created by 권유진 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct MiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
